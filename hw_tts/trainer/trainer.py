7c4663b84a9dec704225ce4710787c18d47cc7d3
alishudi <alexlishudi@gmail.com>
2022-11-27 01:18:01 +0400

    fixing mel logging

--- hw_tts/trainer/trainer.py
+++ hw_tts/trainer/trainer.py
@@ -174,7 +174,7 @@ class Trainer(BaseTrainer):
             for i, phn in tqdm(enumerate(self.encoded_test)):
                 mel, path = synthesis(self.model, phn, self.device, self.waveglow, i, speed)
                 self._log_audio(path, path)
-                image = PIL.Image.open(plot_spectrogram_to_buf(mel.detach().cpu().numpy().transpose(-1, -2)))
+                image = PIL.Image.open(plot_spectrogram_to_buf(mel.detach().cpu().numpy().squeeze(0).transpose(-1, -2)))
                 self.writer.add_image(path, ToTensor()(image))
 
 
