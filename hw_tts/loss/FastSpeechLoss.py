80902c679965afedcd021edb00ca60f06f981bc4
alishudi <alexlishudi@gmail.com>
2022-11-27 00:20:56 +0400

    fix loss arguments

--- hw_tts/loss/FastSpeechLoss.py
+++ hw_tts/loss/FastSpeechLoss.py
@@ -8,7 +8,7 @@ class FastSpeechLoss(nn.Module):
         self.mse_loss = nn.MSELoss()
         self.l1_loss = nn.L1Loss()
 
-    def forward(self, mel, duration_predicted, mel_target, duration_predictor_target):
+    def forward(self, mel, duration_predicted, mel_target, duration_predictor_target, **batch):
         mel_loss = self.mse_loss(mel, mel_target)
 
         duration_predictor_loss = self.l1_loss(duration_predicted,
