c98b84bb3a8b959920688408b3158354143e0a35
alishudi <alexlishudi@gmail.com>
2022-11-27 00:05:03 +0400

    debug

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def main(config):
 
     # build model architecture, then print to console
     model = config.init_obj(config["arch"], module_arch)
-    logger.info(model)
+    # logger.info(model)
 
     # prepare for (multi-device) GPU training
     device, device_ids = prepare_device(config["n_gpu"])
